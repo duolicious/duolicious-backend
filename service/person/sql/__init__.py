b128de10c24ad1ad0de4245f9fab698f741c4c39
duogenesis <136373989+duogenesis@users.noreply.github.com>
2024-01-22 00:15:21 +1100

    Adjust adaptive distance again

--- service/person/sql/__init__.py
+++ service/person/sql/__init__.py
@@ -340,7 +340,7 @@ WITH onboardee_country AS (
         unbounded_age_preference
 ), best_distance AS (
     -- Use a binary search to compute the "furthest distance" search preference
-    -- which causes search results to contain as close as possible to 500 users
+    -- which causes search results to contain as close as possible to 1000 users
     WITH RECURSIVE t(dist, cnt, iters) AS (
         VALUES
             (    0.0,     0.0, 0),
@@ -355,7 +355,7 @@ WITH onboardee_country AS (
                     t
                 ORDER BY
                     iters DESC,
-                    ABS(cnt - 500),
+                    ABS(cnt - 1000),
                     dist DESC
                 LIMIT 2
             ), midpoint AS (
