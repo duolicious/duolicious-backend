334ab49aa4ad1d8b49bcf4f883c6484bb85ce02e
duogenesis <136373989+duogenesis@users.noreply.github.com>
2024-02-29 03:40:57 +1100

    Bring Duolicious back up w/ photocleaner changes (#250)

--- service/cron/photocleaner/sql/__init__.py
+++ service/cron/photocleaner/sql/__init__.py
@@ -1,9 +1,5 @@
 Q_UNUSED_PHOTOS = """
-       SELECT uuid FROM undeleted_photo
-
--- TODO: Delete these
-EXCEPT SELECT uuid FROM photo
-EXCEPT SELECT uuid FROM onboardee_photo
+SELECT uuid FROM undeleted_photo
 """
 
 Q_MARK_PHOTO_DELETED = """
