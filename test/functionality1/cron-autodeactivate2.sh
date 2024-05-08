7761041c3d56094f1b0e483a42fb92e86e213dab
duogenesis <136373989+duogenesis@users.noreply.github.com>
2024-05-08 22:46:40 +1000

    autodeactivate2: 100 days

--- test/functionality1/cron-autodeactivate2.sh
+++ test/functionality1/cron-autodeactivate2.sh
@@ -77,9 +77,9 @@ do_test () {
   local user4id=$(get_id 'will-remain-active3@duolicious.app')
 
   local  days_ago_0=$(db_now as-seconds)
-  local  days_ago_1=$(db_now as-seconds '-  91   days')
-  local  days_ago_2=$(db_now as-seconds '- 101   days')
-  local  days_ago_3=$(db_now as-seconds '- 111   days')
+  local  days_ago_1=$(db_now as-seconds '-  81   days')
+  local  days_ago_2=$(db_now as-seconds '-  91   days')
+  local  days_ago_3=$(db_now as-seconds '- 101   days')
 
   q "
   insert into last (server, username, seconds, state)
