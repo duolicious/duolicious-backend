duolicious/duolicious-backend

commit 2a77c22b6ffbc182434acd02fbe6875cc9443c72

Patch some vulnerabilities (#296)

service/chat/__init__.py
@@ -1,3 +1,4 @@
+# TODO: Conversations need to be migrated
 from database.asyncdatabase import api_tx, chat_tx, check_connections_forever
 from lxml import etree
 import asyncio
@@ -22,42 +23,57 @@ RETURNING hash
 """
 
 Q_IS_SKIPPED = """
+WITH from_username AS (
+    SELECT id FROM person WHERE uuid = %(from_username)s
+), to_username AS (
+    SELECT id FROM person WHERE uuid = %(to_username)s
+)
 SELECT
     1
 FROM
     skipped
 WHERE
     (
-        subject_person_id = %(from_username)s AND
-        object_person_id  = %(to_username)s
+        subject_person_id = (SELECT id FROM from_username) AND
+        object_person_id  = (SELECT id FROM to_username)
     )
 OR
     (
-        subject_person_id = %(to_username)s AND
-        object_person_id  = %(from_username)s
+        subject_person_id = (SELECT id FROM to_username) AND
+        object_person_id  = (SELECT id FROM from_username)
     )
 LIMIT 1
 """
 
 Q_SET_MESSAGED = """
-WITH can_insert AS (
+WITH subject_person_id AS (
+    SELECT
+        id
+    FROM
+        person
+    WHERE
+        uuid = %(subject_person_id)s
+), object_person_id AS (
+    SELECT
+        id
+    FROM
+        person
+    WHERE
+        uuid = %(object_person_id)s
+), can_insert AS (
     SELECT
         1
     WHERE
-        EXISTS (
-            SELECT 1 FROM person WHERE id = %(subject_person_id)s
-        )
-        AND EXISTS (
-            SELECT 1 FROM person WHERE id = %(object_person_id)s
-        )
+        EXISTS (SELECT 1 FROM subject_person_id)
+    AND EXISTS (SELECT 1 FROM object_person_id)
 ), insertion AS (
     INSERT INTO messaged (
         subject_person_id,
         object_person_id
     )
     SELECT
-        %(subject_person_id)s,
-        %(object_person_id)s
+        (SELECT id FROM subject_person_id),
+        (SELECT id FROM object_person_id)
     WHERE EXISTS (
         SELECT
             1
@@ -73,13 +89,13 @@ FROM
 """
 
 Q_SET_TOKEN = """
-INSERT INTO duo_push_token (
-    username,
-    token
-) VALUES (
-    %(username)s,
+INSERT INTO duo_push_token (username, token)
+VALUES (
+    (SELECT id FROM person WHERE id = %(username)s),
     %(token)s
-) ON CONFLICT (username) DO UPDATE SET
+)
+ON CONFLICT (username)
+DO UPDATE SET
     token = EXCLUDED.token
 """
 
@@ -116,15 +132,17 @@ def get_message_attrs(message_xml):
         if body is not None:
             maybe_message_body = body.text
 
-        return (
-            root.attrib.get('id'),
-            root.attrib.get('to'),
-            do_check_uniqueness,
-            maybe_message_body)
+        _id = root.attrib.get('id')
+        assert _id is not None
+
+        to = root.attrib.get('to')
+        assert to is not None
+
+        return (True, _id, to, do_check_uniqueness, maybe_message_body)
     except Exception as e:
         pass
 
-    return None, None, None, None
+    return False, None, None, None, None
 
 def normalize_message(message_str):
     message_str = message_str.lower()
@@ -187,8 +205,8 @@ async def is_message_unique(message_str):
 
 async def is_message_blocked(username, to_jid):
     try:
-        from_username = int(username)
-        to_username = int(to_jid.split('@')[0])
+        from_username = username
+        to_username = to_jid.split('@')[0]
 
         params = dict(
             from_username=from_username,
@@ -206,8 +224,8 @@ async def is_message_blocked(username, to_jid):
     return False
 
 async def set_messaged(username, to_jid):
-    from_username = int(username)
-    to_username = int(to_jid.split('@')[0])
+    from_username = username
+    to_username = to_jid.split('@')[0]
 
     params = dict(
         subject_person_id=from_username,
@@ -251,42 +269,47 @@ async def process_duo_message(message_xml, username):
     if await maybe_register(message_xml, username):
         return ['<duo_registration_successful />'], []
 
-    id, to_jid, do_check_uniqueness, maybe_message_body = get_message_attrs(
-        message_xml)
+    (
+        is_message,
+        id,
+        to_jid,
+        do_check_uniqueness,
+        maybe_message_body,
+    ) = get_message_attrs(message_xml)
 
-    if id and maybe_message_body and is_message_too_long(maybe_message_body):
+    if not is_message:
+        return [], [message_xml]
+
+    if maybe_message_body and is_message_too_long(maybe_message_body):
         return [f'<duo_message_too_long id="{id}"/>'], []
 
-    if id and await is_message_blocked(username, to_jid):
+    if await is_message_blocked(username, to_jid):
         return [f'<duo_message_blocked id="{id}"/>'], []
 
-    if id and maybe_message_body and do_check_uniqueness and \
+    if maybe_message_body and do_check_uniqueness and \
             not await is_message_unique(maybe_message_body):
         return [f'<duo_message_not_unique id="{id}"/>'], []
 
-    if id:
-        if await set_messaged(username, to_jid):
-            return (
-                [
-                    f'<duo_message_delivered id="{id}"/>'
-                ],
-                [
-                    message_xml,
-                    f"<iq id='{duohash.duo_uuid()}' type='set'>"
-                    f"  <query"
-                    f"    xmlns='erlang-solutions.com:xmpp:inbox:0#conversation'"
-                    f"    jid='{to_jid}'"
-                    f"  >"
-                    f"    <box>chats</box>"
-                    f"  </query>"
-                    f"</iq>"
+    if await set_messaged(username, to_jid):
+        return (
+            [
+                f'<duo_message_delivered id="{id}"/>'
+            ],
+            [
+                message_xml,
+                f"<iq id='{duohash.duo_uuid()}' type='set'>"
+                f"  <query"
+                f"    xmlns='erlang-solutions.com:xmpp:inbox:0#conversation'"
+                f"    jid='{to_jid}'"
+                f"  >"
+                f"    <box>chats</box>"
+                f"  </query>"
+                f"</iq>"
 
-                ]
-            )
-        else:
-            return [], []
-    else:
-        return [], [message_xml]
+            ]
+        )
+
+    return [], []
 
 async def process(src, dst, username):
     try:

duotypes/__init__.py
@@ -81,6 +81,7 @@ class SessionInfo(BaseModel):
     email: str
     session_token_hash: str
     person_id: Optional[int]
+    person_uuid: Optional[str]
     onboarded: bool
     signed_in: bool
 
@@ -255,7 +256,7 @@ class PostSearchFilterAnswer(BaseModel):
     accept_unanswered: bool
 
 class PostInboxInfo(BaseModel):
-    person_ids: List[int]
+    person_uuids: List[str]
 
 class PostJoinClub(BaseModel):
     name: constr(pattern=CLUB_PATTERN, min_length=1, max_length=CLUB_MAX_LEN)

service/application/__init__.py
@@ -151,9 +151,9 @@ def get_me_by_session(s: t.SessionInfo):
 def get_me_by_id(person_id: str):
     return person.get_me(person_id_as_str=person_id)
 
-@aget('/prospect-profile/<int:prospect_person_id>')
-def get_prospect_profile(s: t.SessionInfo, prospect_person_id: int):
-    return person.get_prospect_profile(s, prospect_person_id)
+@aget('/prospect-profile/<prospect_uuid>')
+def get_prospect_profile(s: t.SessionInfo, prospect_uuid: int):
+    return person.get_prospect_profile(s, prospect_uuid)
 
 @apost('/skip/<int:prospect_person_id>')
 @validate(t.PostSkip)

service/cron/notifications/__init__.py
@@ -33,8 +33,7 @@ print('Hello from cron module: notifications')
 
 @dataclass
 class PersonNotification:
-    person_id: int
-    username: str
+    person_uuid: int
     last_intro_notification_seconds: int
     last_chat_notification_seconds: int
     last_intro_seconds: int
@@ -78,7 +77,7 @@ def do_send_email_notification(row: PersonNotification):
     return do_send_notification(row) and not is_example
 
 async def delete_mobile_token(row: PersonNotification):
-    params = dict(username=row.username)
+    params = dict(username=row.person_uuid)
 
     async with chat_tx() as tx:
         await tx.execute(Q_DELETE_MOBILE_TOKEN, params)
@@ -149,7 +148,7 @@ async def send_notification(row: PersonNotification):
         return send_email_notification(row)
 
 async def update_last_notification_time(row: PersonNotification):
-    params = dict(username=row.username)
+    params = dict(username=row.person_uuid)
 
     async with chat_tx() as tx:
         if row.has_intro:
@@ -172,14 +171,14 @@ async def send_notifications_once():
     async with api_tx() as tx:
         cur_notification_settings = await tx.execute(
             Q_NOTIFICATION_SETTINGS,
-            params=dict(ids=[r['person_id'] for r in rows_unread_inbox])
+            params=dict(ids=[r['person_uuid'] for r in rows_unread_inbox])
         )
         rows_notification_settings = await cur_notification_settings.fetchall()
 
     joined = join_lists_of_dicts(
         rows_unread_inbox,
         rows_notification_settings,
-        'person_id',
+        'person_uuid',
     )
     person_notifications = [PersonNotification(**j) for j in joined]
 

service/cron/notifications/sql/__init__.py
@@ -27,8 +27,7 @@ WITH ten_days_ago AS (
         luser
 ), inbox_second_pass AS (
     SELECT
-        inbox_first_pass.username,
-        inbox_first_pass.username::int AS person_id,
+        inbox_first_pass.username AS person_uuid,
         inbox_first_pass.last_intro_seconds,
         inbox_first_pass.last_chat_seconds,
         COALESCE(duo_last_notification.intro_seconds, 0) AS last_intro_notification_seconds,
@@ -86,8 +85,7 @@ WITH ten_days_ago AS (
         duo_last_notification.username = inbox_first_pass.username
 )
 SELECT
-    inbox_second_pass.username,
-    inbox_second_pass.username::int AS person_id,
+    inbox_second_pass.person_uuid,
     last_intro_seconds,
     last_chat_seconds,
     last_intro_notification_seconds,
@@ -100,7 +98,7 @@ FROM
 LEFT JOIN
     duo_push_token
 ON
-    duo_push_token.username = inbox_second_pass.username
+    duo_push_token.username = inbox_second_pass.person_uuid
 WHERE
     has_intro
 OR
@@ -109,7 +107,7 @@ OR
 
 Q_NOTIFICATION_SETTINGS = """
 SELECT
-    id AS person_id,
+    uuid::text AS person_uuid,
     name,
     email,
     (
@@ -138,7 +136,7 @@ SELECT
     )
 FROM person
 WHERE
-    id = ANY(%(ids)s)
+    uuid = ANY(%(ids)s)
 AND
     activated
 """

service/person/__init__.py
@@ -326,13 +326,14 @@ def post_check_otp(req: t.PostCheckOtp, s: t.SessionInfo):
         if not row:
             return 'Invalid OTP', 401
 
-    params = dict(person_id=row['person_id'])
+    params = dict(person_uuid=row['person_uuid'])
 
     with chat_tx() as tx:
         tx.execute(Q_UPDATE_LAST, params)
 
     return dict(
         person_id=row['person_id'],
+        person_uuid=row['person_uuid'],
         onboarded=row['person_id'] is not None,
         units=row['units'],
     )
@@ -351,6 +352,7 @@ def post_check_session_token(s: t.SessionInfo):
         if row:
             return dict(
                 person_id=s.person_id,
+                person_uuid=s.person_uuid,
                 onboarded=s.onboarded,
                 units=row['units'],
             )
@@ -524,7 +526,10 @@ def post_finish_onboarding(s: t.SessionInfo):
     with api_tx() as tx:
         row = tx.execute(Q_FINISH_ONBOARDING, params=api_params).fetchone()
 
-    chat_params = dict(person_id=row['person_id'])
+    chat_params = dict(
+        person_id=row['person_id'],
+        person_uuid=row['person_uuid'],
+    )
 
     with chat_tx() as tx:
         tx.execute(Q_INSERT_LAST, params=chat_params)
@@ -566,10 +571,10 @@ def get_me(
     except:
         return '', 404
 
-def get_prospect_profile(s: t.SessionInfo, prospect_person_id: int):
+def get_prospect_profile(s: t.SessionInfo, prospect_uuid):
     params = dict(
         person_id=s.person_id,
-        prospect_person_id=prospect_person_id,
+        prospect_uuid=prospect_uuid,
     )
 
     with api_tx('READ COMMITTED') as tx:
@@ -683,7 +688,7 @@ def get_compare_answers(
 def post_inbox_info(req: t.PostInboxInfo, s: t.SessionInfo):
     params = dict(
         person_id=s.person_id,
-        prospect_person_ids=req.person_ids
+        prospect_person_uuids=req.person_uuids
     )
 
     with api_tx('READ COMMITTED') as tx:

service/person/sql/__init__.py
@@ -261,32 +261,45 @@ RETURNING email
 
 Q_MAYBE_SIGN_IN = """
 WITH valid_session AS (
-    UPDATE duo_session
-    SET signed_in = TRUE
+    UPDATE
+        duo_session
+    SET
+        signed_in = TRUE
     WHERE
         session_token_hash = %(session_token_hash)s AND
         otp = %(otp)s AND
         otp_expiry > NOW()
-    RETURNING person_id, email
+    RETURNING
+        person_id,
+        email
 ), existing_person AS (
-    UPDATE person
+    UPDATE
+        person
     SET
         activated = TRUE,
         sign_in_count = sign_in_count + 1,
         sign_in_time = NOW()
-    FROM valid_session
-    WHERE person.id = person_id
-    RETURNING person.id, person.unit_id
+    FROM
+        valid_session
+    WHERE
+        person.id = person_id
+    RETURNING
+        person.id,
+        person.uuid AS person_uuid,
+        person.unit_id
 ), new_onboardee AS (
     INSERT INTO onboardee (
         email
     )
-    SELECT email
-    FROM valid_session
+    SELECT
+        email
+    FROM
+        valid_session
     WHERE NOT EXISTS (SELECT 1 FROM existing_person)
 )
 SELECT
     person_id,
+    person_uuid,
     email,
     (SELECT name FROM unit WHERE id = existing_person.unit_id) AS units
 FROM
@@ -370,7 +383,13 @@ WITH onboardee_country AS (
         3 AS intros_notification
     FROM onboardee
     WHERE email = %(email)s
-    RETURNING id, email, unit_id, coordinates, date_of_birth
+    RETURNING
+        id,
+        person.uuid,
+        email,
+        unit_id,
+        coordinates,
+        date_of_birth
 ), best_age AS (
     WITH new_person_age AS (
         SELECT
@@ -542,7 +561,7 @@ WITH onboardee_country AS (
     SELECT
         new_person.id,
         position,
-        uuid
+        onboardee_photo.uuid
     FROM onboardee_photo
     JOIN new_person
     ON onboardee_photo.email = new_person.email
@@ -678,14 +697,22 @@ WITH onboardee_country AS (
     WHERE email = %(email)s
 )
 SELECT
-    id AS person_id,
+    new_person.id AS person_id,
+    new_person.uuid AS person_uuid,
     (SELECT name FROM unit WHERE unit.id = new_person.unit_id) AS units
 FROM
     new_person
 """
 
 Q_SELECT_PROSPECT_PROFILE = """
-WITH prospect AS (
+WITH prospect_person_id AS (
+    SELECT
+        id
+    FROM
+        person
+    WHERE
+        uuid = %(prospect_uuid)s
+), prospect AS (
     SELECT
         *,
         (
@@ -702,19 +729,19 @@ WITH prospect AS (
     FROM
         person AS p
     WHERE
-        id = %(prospect_person_id)s
+        id = (SELECT id FROM prospect_person_id)
     AND
         activated
     AND (
             NOT hide_me_from_strangers
         OR
-            %(prospect_person_id)s = %(person_id)s
+            (SELECT id FROM prospect_person_id) = %(person_id)s
         OR
             EXISTS (
                 SELECT 1
                 FROM messaged
                 WHERE
-                    messaged.subject_person_id = %(prospect_person_id)s
+                    messaged.subject_person_id = (SELECT id FROM prospect_person_id)
                 AND
                     messaged.object_person_id = %(person_id)s
             )
@@ -724,7 +751,7 @@ WITH prospect AS (
             SELECT 1
             FROM skipped
             WHERE
-                subject_person_id = %(prospect_person_id)s AND
+                subject_person_id = (SELECT id FROM prospect_person_id) AND
                 object_person_id  = %(person_id)s
             LIMIT 1
         )
@@ -748,7 +775,7 @@ WITH prospect AS (
 ), photo_uuids AS (
     SELECT COALESCE(json_agg(uuid ORDER BY position), '[]'::json) AS j
     FROM photo
-    WHERE person_id = %(prospect_person_id)s
+    WHERE person_id = (SELECT id FROM prospect_person_id)
 ), gender AS (
     SELECT gender.name AS j
     FROM gender JOIN prospect ON gender_id = gender.id
@@ -808,7 +835,7 @@ WITH prospect AS (
             FROM skipped
             WHERE
                 subject_person_id = %(person_id)s AND
-                object_person_id  = %(prospect_person_id)s
+                object_person_id  = (SELECT id FROM prospect_person_id)
         ) AS j
 ), clubs AS (
     SELECT
@@ -823,7 +850,7 @@ WITH prospect AS (
     AND
         person_club_.person_id = %(person_id)s
     WHERE
-        prospect_person_club.person_id = %(prospect_person_id)s
+        prospect_person_club.person_id = (SELECT id FROM prospect_person_id)
     ORDER BY
         is_mutual DESC,
         club_name
@@ -854,6 +881,7 @@ WITH prospect AS (
 )
 SELECT
     json_build_object(
+        'person_id',              (SELECT id            FROM prospect_person_id),
         'photo_uuids',            (SELECT j             FROM photo_uuids),
         'name',                   (SELECT name          FROM prospect),
         'age',                    (SELECT age           FROM prospect),
@@ -992,6 +1020,7 @@ Q_INBOX_INFO = """
 WITH person_info AS (
     SELECT
         id_table.id AS person_id,
+        id_table.uuid AS person_uuid,
         prospect.id IS NULL AS is_prospect_deleted,
         COALESCE(prospect.activated, FALSE) AS is_prospect_activated,
         prospect.name AS name,
@@ -1042,7 +1071,28 @@ WITH person_info AS (
         ) AS prospect_skipped_person
     FROM
         (
-            SELECT UNNEST(%(prospect_person_ids)s) AS id
+            SELECT id, uuid
+            FROM person
+            WHERE uuid = ANY(%(prospect_person_uuids)s::uuid[])
+
+            UNION
+
+            SELECT
+                id,
+                uuid
+            FROM
+                person
+            JOIN
+                messaged
+            ON
+                messaged.subject_person_id = %(person_id)s
+            AND
+                messaged.object_person_id = person.id
+            OR
+                messaged.subject_person_id = person.id
+            AND
+                messaged.object_person_id = %(person_id)s
+            WHERE %(prospect_person_uuids)s::uuid[] = array[]::uuid[]
         ) AS id_table
     LEFT JOIN
         person AS prospect
@@ -1057,6 +1107,7 @@ WITH person_info AS (
 )
 SELECT
     person_id,
+    person_uuid,
     CASE
         WHEN is_prospect_activated AND NOT prospect_skipped_person
         THEN
@@ -1844,7 +1895,7 @@ Q_INSERT_LAST = """
 INSERT INTO
     last (server, username, seconds, state)
 VALUES
-    ('duolicious.app', %(person_id)s, EXTRACT(EPOCH FROM NOW())::BIGINT, '')
+    ('duolicious.app', %(person_uuid)s::text, EXTRACT(EPOCH FROM NOW())::BIGINT, '')
 """
 
 Q_UPDATE_LAST = """
@@ -1855,5 +1906,5 @@ SET
 WHERE
     server = 'duolicious.app'
 AND
-    username = %(person_id)s::TEXT
+    username = %(person_uuid)s::TEXT
 """

service/search/sql/__init__.py
@@ -42,6 +42,8 @@ WITH searcher AS MATERIALIZED (
     SELECT
         id AS prospect_person_id,
 
+        uuid AS prospect_uuid,
+
         name,
         EXTRACT(YEAR FROM AGE(date_of_birth)) AS age,
 
@@ -196,6 +198,7 @@ WITH searcher AS MATERIALIZED (
 ), prospects_second_pass AS (
     SELECT
         prospect_person_id,
+        prospect_uuid,
         (
             SELECT uuid
             FROM photo
@@ -419,6 +422,7 @@ WITH searcher AS MATERIALIZED (
         searcher_person_id,
         position,
         prospect_person_id,
+        prospect_uuid,
         profile_photo_uuid,
         has_mutual_club,
         name,
@@ -436,6 +440,7 @@ WITH searcher AS MATERIALIZED (
                 match_percentage DESC
         ) AS position,
         prospect_person_id,
+        prospect_uuid,
         profile_photo_uuid,
         has_mutual_club,
         name,
@@ -448,6 +453,7 @@ WITH searcher AS MATERIALIZED (
 )
 SELECT
     prospect_person_id,
+    prospect_uuid,
     profile_photo_uuid,
     name,
     age,
@@ -483,6 +489,7 @@ LIMIT
 Q_CACHED_SEARCH = """
 SELECT
     prospect_person_id,
+    prospect_uuid,
     profile_photo_uuid,
     name,
     age,
@@ -529,6 +536,7 @@ WITH searcher AS (
 )
 SELECT
     prospect_person_id,
+    prospect_uuid,
     profile_photo_uuid,
     name,
     age,

commit d1ac424762be593f9875bab54cc32075a3080939

Fix notifications broken in #296

service/chat/__init__.py
@@ -91,7 +91,7 @@ FROM
 Q_SET_TOKEN = """
 INSERT INTO duo_push_token (username, token)
 VALUES (
-    (SELECT id FROM person WHERE id = %(username)s),
+    %(username)s,
     %(token)s
 )
 ON CONFLICT (username)