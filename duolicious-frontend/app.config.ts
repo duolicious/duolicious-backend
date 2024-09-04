b6fdfe4339f60508027c89d1e167d0b36c42bfd8
duogenesis <136373989+duogenesis@users.noreply.github.com>
2024-09-04 10:56:27 +1000

    Version 18 (#397)

--- duolicious-frontend/app.config.ts
+++ duolicious-frontend/app.config.ts
@@ -6,7 +6,7 @@ import { ExpoConfig } from 'expo/config';
 const config: ExpoConfig = {
   name: 'Duolicious',
   slug: 'duolicious',
-  version: "17.0.0",
+  version: "18.0.0",
   orientation: "portrait",
   icon: './assets/icon.png',
   splash: {
@@ -45,7 +45,7 @@ const config: ExpoConfig = {
   android: {
     googleServicesFile: "./google-services.json",
     package: "app.duolicious",
-    versionCode: 17,
+    versionCode: 18,
     adaptiveIcon: {
       foregroundImage: "./assets/adaptive-icon.png",
       backgroundColor: "#ffffff"
