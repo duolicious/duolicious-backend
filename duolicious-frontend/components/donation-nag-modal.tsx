246113ff80d6a1239269b6e59f33caac5cfd828d
duogenesis <136373989+duogenesis@users.noreply.github.com>
2024-09-09 13:13:25 +1000

    Disable donation nag modal (#399)

--- duolicious-frontend/components/donation-nag-modal.tsx
+++ duolicious-frontend/components/donation-nag-modal.tsx
@@ -14,6 +14,8 @@ import { signedInUser } from '../App';
 import { api } from '../api/api';
 
 const DonationNagModal = () => {
+  return null; // TODO
+
   const name = signedInUser?.name;
   const estimatedEndDate = signedInUser?.estimatedEndDate;
   const doShowDonationNag = signedInUser?.doShowDonationNag;
