3b5bc91c96e1129e621d02002b2b6a05aaf82afb
duogenesis <136373989+duogenesis@users.noreply.github.com>
2024-09-10 00:03:33 +1000

    Refine donation nag screen (#487)

--- test/functionality1/donation.sh
+++ test/functionality1/donation.sh
@@ -7,9 +7,15 @@ source ../util/setup.sh
 
 set -xe
 
+date_20_days_later=$(
+  q "select now() + interval '20 days'")
+date_10_days_ago=$(
+  q "select now() - interval '10 days'")
+
 q "delete from person"
 q "delete from club"
 q "delete from banned_person"
+q "update funding set estimated_end_date = '$date_20_days_later'"
 
 ../util/create-user.sh user1 0 0
 
@@ -20,42 +26,42 @@ response=$( jc POST /check-otp -d '{ "otp": "000000" }' )
 
 [[ "$(jq -r '.name' <<< "$response")" = user1 ]]
 [[ "$(jq -r '.do_show_donation_nag' <<< "$response")" = false ]]
-[[ "$(jq -r '.estimated_end_date' <<< "$response")" = '2024-09-17 15:02:10.866+00' ]]
+[[ "$(jq -r '.estimated_end_date' <<< "$response")" = "$date_20_days_later" ]]
 
-# Satisfy conditions for .do_show_donation_nag to be true
+echo Satisfy conditions for .do_show_donation_nag to be true
 q "update person set count_answers = 25"
-q "update person set last_nag_time = now() - interval '2 days'"
-q "update person set sign_up_time = now() - interval '2 days'"
+q "update person set last_nag_time = '$date_10_days_ago'"
+q "update person set sign_up_time = '$date_10_days_ago'"
 
 response=$(c POST /check-session-token)
 [[ "$(jq -r '.do_show_donation_nag' <<< "$response")" = true ]]
 
-# Remove condition 1
+echo Remove condition 1
 q "update person set count_answers = 25"
-q "update person set last_nag_time = now() - interval '2 days'"
-q "update person set sign_up_time = now() - interval '2 days'"
+q "update person set last_nag_time = '$date_10_days_ago'"
+q "update person set sign_up_time = '$date_10_days_ago'"
 
 q "update person set count_answers = 24"
 
 response=$(c POST /check-session-token)
 [[ "$(jq -r '.do_show_donation_nag' <<< "$response")" = false ]]
 
-# Remove condition 2
+echo Remove condition 2
 q "update person set count_answers = 25"
-q "update person set last_nag_time = now() - interval '2 days'"
-q "update person set sign_up_time = now() - interval '2 days'"
+q "update person set last_nag_time = '$date_10_days_ago'"
+q "update person set sign_up_time = '$date_10_days_ago'"
 
-q "update person set last_nag_time = now()"
+q "update person set last_nag_time = '$date_10_days_ago'::TIMESTAMP + interval '1 day'"
 
 response=$(c POST /check-session-token)
 [[ "$(jq -r '.do_show_donation_nag' <<< "$response")" = false ]]
 
-# Remove condition 3
+echo Remove condition 3
 q "update person set count_answers = 25"
-q "update person set last_nag_time = now() - interval '2 days'"
-q "update person set sign_up_time = now() - interval '2 days'"
+q "update person set last_nag_time = '$date_10_days_ago'"
+q "update person set sign_up_time = '$date_10_days_ago'"
 
-q "update person set sign_up_time = now()"
+q "update person set sign_up_time = '$date_10_days_ago'::TIMESTAMP + interval '1 day'"
 
 response=$(c POST /check-session-token)
 [[ "$(jq -r '.do_show_donation_nag' <<< "$response")" = false ]]
