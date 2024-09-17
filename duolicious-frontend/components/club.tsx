3de21729731ef5f0ca82356108900fbe9eb6e6b5
duogenesis <136373989+duogenesis@users.noreply.github.com>
2024-09-18 03:22:41 +1000

    Clubs are styled Basics (#412)

--- duolicious-frontend/components/club.tsx
+++ duolicious-frontend/components/club.tsx
@@ -1,69 +1,31 @@
-import {
-  Text,
-  View,
-} from 'react-native';
-import { DefaultText } from './default-text';
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
-import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { Basic, Basics } from './basic';
 
 const Club = ({
   name,
   isMutual,
   style,
+  textStyle,
 }: {
   name: string,
   isMutual: boolean,
-  style?: any
+  style?: any,
+  textStyle?: any,
 }) => {
   return (
-    <View
-      style={[
-        {
-          borderColor: (
-            isMutual ? 'rgba(119, 0, 255, 0.1)' : 'rgba(0, 0, 0, 0.1)'),
-          borderWidth: 1,
-          borderRadius: 999,
-          paddingTop: 5,
-          paddingBottom: 5,
-          paddingLeft: 10,
-          paddingRight: 10,
-          marginRight: 5,
-          marginBottom: 5,
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexDirection: 'row',
-          backgroundColor: (
-            isMutual ? 'rgba(235, 217, 255, 0.7)' : 'rgba(255, 255, 255, 0.7)'),
-        },
-        style,
-      ]}
+    <Basic
+      textStyle={{
+        fontFamily: isMutual ? 'TruenoBold' : 'Trueno',
+        fontWeight: isMutual ? '900' : '400',
+        fontSize: 15,
+        ...textStyle,
+      }}
     >
-      <Text
-        style={{
-          fontFamily: 'Trueno',
-          color: isMutual ? '#70f' : 'black',
-          fontWeight: isMutual ? '900' : '400',
-        }}
-      >
-        {name}
-      </Text>
-    </View>
+      {name}
+    </Basic>
   );
 };
 
-const Clubs = ({children}) => {
-  return (
-    <View
-      style={{
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-      }}
-    >
-      {children}
-    </View>
-  );
-};
+const Clubs = Basics;
 
 export {
   Club,
