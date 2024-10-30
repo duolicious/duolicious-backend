ca5abc8cfd000ae30780cee716fa6b65e3b172a5
duogenesis <136373989+duogenesis@users.noreply.github.com>
2024-10-31 01:12:45 +1100

    Clean up migrations from #500

--- migrations.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE photo
-    ADD COLUMN IF NOT EXISTS extra_exts TEXT[] NOT NULL DEFAULT '{}';
-
-ALTER TABLE onboardee_photo
-    ADD COLUMN IF NOT EXISTS extra_exts TEXT[] NOT NULL DEFAULT '{}';
