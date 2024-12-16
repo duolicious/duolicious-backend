e2b57225101b5f822bf01a17dd60fe9e02b9ae57
duogenesis <136373989+duogenesis@users.noreply.github.com>
2024-12-17 02:37:23 +1100

    Disable spam detection for trusted accounts

--- test/functionality4/xmpp-spam.sh
+++ test/functionality4/xmpp-spam.sh
@@ -22,15 +22,19 @@ q "delete from intro_hash" duo_chat
 
 ../util/create-user.sh user1 0 0
 ../util/create-user.sh user2 0 0
+../util/create-user.sh user3 0 0
 
 assume_role user1 ; user1token=$SESSION_TOKEN
 assume_role user2 ; user2token=$SESSION_TOKEN
+assume_role user3 ; user3token=$SESSION_TOKEN
 
 user1uuid=$(get_uuid 'user1@example.com')
 user2uuid=$(get_uuid 'user2@example.com')
+user3uuid=$(get_uuid 'user3@example.com')
 
 user1id=$(get_id 'user1@example.com')
 user2id=$(get_id 'user2@example.com')
+user3id=$(get_id 'user3@example.com')
 
 
 
@@ -60,7 +64,6 @@ curl -X POST http://localhost:3000/send -H "Content-Type: application/xml" -d "
     from='$user1uuid@duolicious.app'
     to='$user2uuid@duolicious.app'
     id='id1'
-    check_uniqueness='false'
     xmlns='jabber:client'>
   <body>You should join discord.gg/spaghetti</body>
   <request xmlns='urn:xmpp:receipts'/>
@@ -90,7 +93,6 @@ curl -X POST http://localhost:3000/send -H "Content-Type: application/xml" -d "
     from='$user1uuid@duolicious.app'
     to='$user2uuid@duolicious.app'
     id='id1'
-    check_uniqueness='false'
     xmlns='jabber:client'>
   <body>damn I want to volunteer to walk puppies</body>
   <request xmlns='urn:xmpp:receipts'/>
@@ -134,7 +136,6 @@ curl -X POST http://localhost:3000/send -H "Content-Type: application/xml" -d "
     from='$user2uuid@duolicious.app'
     to='$user1uuid@duolicious.app'
     id='id1'
-    check_uniqueness='false'
     xmlns='jabber:client'>
   <body>You should join discord.gg/spaghetti</body>
   <request xmlns='urn:xmpp:receipts'/>
@@ -151,3 +152,44 @@ sleep 3 # MongooseIM takes some time to flush messages to the DB
 curl -sX GET http://localhost:3000/pop | grep -qF '<duo_message_delivered id="id1"/>'
 
 [[ "$(q "select count(*) from mam_message" duo_chat)" = 4 ]]
+
+
+
+curl -X POST http://localhost:3000/config -H "Content-Type: application/json" -d '{
+  "service": "ws://chat:5443",
+  "domain": "duolicious.app",
+  "resource": "testresource",
+  "username": "'$user3uuid'",
+  "password": "'$user3token'"
+}'
+
+
+
+sleep 3
+
+echo A potential spam message is allowed for trusted accounts
+
+q "update person set sign_up_time = now() - interval '7 days' where uuid = '$user3uuid'"
+
+curl -X POST http://localhost:3000/send -H "Content-Type: application/xml" -d "
+<message
+    type='chat'
+    from='$user3uuid@duolicious.app'
+    to='$user2uuid@duolicious.app'
+    id='id1'
+    xmlns='jabber:client'>
+  <body>You should also join discord.gg/spaghetti</body>
+  <request xmlns='urn:xmpp:receipts'/>
+</message>
+"
+
+sleep 3 # MongooseIM takes some time to flush messages to the DB
+
+[[ "$(q "select count(*) from messaged where \
+    subject_person_id = $user3id and \
+    object_person_id = $user2id")" = 1 ]]
+[[ "$(q "select count(*) from messaged")" = 3 ]]
+
+curl -sX GET http://localhost:3000/pop | grep -qF '<duo_message_delivered id="id1"/>'
+
+[[ "$(q "select count(*) from mam_message" duo_chat)" = 6 ]]
