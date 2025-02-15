a4b0b9276ef19cdb63b9ca2c0b7d55cc8ddc6566
duogenesis <136373989+duogenesis@users.noreply.github.com>
2025-02-16 06:52:25 +1100

    Render Tenor gifs in chat (#623)

--- duolicious-frontend/components/speech-bubble.tsx
+++ duolicious-frontend/components/speech-bubble.tsx
@@ -3,12 +3,13 @@ import {
   useState,
 } from 'react';
 import {
+  ActivityIndicator,
   Pressable,
   View,
 } from 'react-native';
 import { DefaultText } from './default-text';
 import { longFriendlyTimestamp } from '../util/util';
-import { Image } from 'expo-image';
+import { useImage, Image } from 'expo-image';
 import { IMAGES_URL } from '../env/env';
 
 type Props = {
@@ -18,20 +19,33 @@ type Props = {
   imageUuid: string | null | undefined,
 };
 
-const isEmojiOnly = (str: string) => {
+const isSafeImageUrl = (str: string): boolean => {
+  const urlRegex = /^https:\/\/media\.tenor\.com\/\S+\.(gif|webp)$/i;
+  return urlRegex.test(str);
+};
+
+const isEmojiOnly = (str: string): boolean => {
   const emojiRegex = /^\p{Emoji_Presentation}+$/u;
   return emojiRegex.test(str);
 }
 
 const SpeechBubble = (props: Props) => {
   const [showTimestamp, setShowTimestamp] = useState(false);
+  const [speechBubbleImageError, setSpeechBubbleImageError] = useState(false);
 
   const onPress = useCallback(() => {
     setShowTimestamp(t => !t);
   }, [setShowTimestamp]);
 
+  const doRenderUrlAsImage = (
+    isSafeImageUrl(props.text) &&
+    !speechBubbleImageError
+  );
+
   const backgroundColor = (() => {
-    if (isEmojiOnly(props.text)) {
+    if (doRenderUrlAsImage) {
+      return 'transparent';
+    } else if (isEmojiOnly(props.text)) {
       return 'transparent';
     } else if (props.fromCurrentUser) {
       return '#70f';
@@ -56,7 +70,11 @@ const SpeechBubble = (props: Props) => {
           flexDirection: 'row',
           gap: 5,
           alignItems: 'flex-end',
-          maxWidth: '80%',
+          ...(doRenderUrlAsImage ? {
+            width: '66%',
+          }: {
+            maxWidth: '80%',
+          })
         }}
       >
         {props.imageUuid &&
@@ -75,19 +93,31 @@ const SpeechBubble = (props: Props) => {
           style={{
             borderRadius: 10,
             backgroundColor: backgroundColor,
-            padding: 10,
-            flexShrink: 1,
+            ...(doRenderUrlAsImage ? {
+              width: '100%',
+            }: {
+              padding: 10,
+              flexShrink: 1,
+            })
           }}
         >
-          <DefaultText
-            selectable={true}
-            style={{
-              color: props.fromCurrentUser ? 'white' : 'black',
-              fontSize: isEmojiOnly(props.text) ? 50 : 15,
-            }}
-          >
-            {props.text}
-          </DefaultText>
+          {doRenderUrlAsImage &&
+            <SpeechBubbleImage
+              uri={props.text}
+              onError={() => setSpeechBubbleImageError(true)}
+            />
+          }
+          {!doRenderUrlAsImage &&
+            <DefaultText
+              selectable={true}
+              style={{
+                color: props.fromCurrentUser ? 'white' : 'black',
+                fontSize: isEmojiOnly(props.text) ? 50 : 15,
+              }}
+            >
+              {props.text}
+            </DefaultText>
+          }
         </Pressable>
       </View>
       {showTimestamp &&
@@ -104,6 +134,63 @@ const SpeechBubble = (props: Props) => {
         </DefaultText>
       }
     </View>
+  );
+};
+
+const SpeechBubbleImage = ({
+  uri,
+  onError,
+}: {
+  uri: string
+  onError: () => void
+}) => {
+  const image = useImage(uri, { onError });
+
+  if (!image) {
+    return (
+      <View
+        style={{
+          // The dimensions are chosen so the height of the placeholder will be
+          // less than or equal to the rendered image. Otherwise
+          // scroll-to-bottom will be broken in the case that the image loads
+          // after the conversation screen already scrolled to the bottom.
+          width: '100%',
+          aspectRatio: 1,
+
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          borderRadius: 10,
+          gap: 12,
+        }}
+      >
+        <ActivityIndicator size="large" color="white" />
+        <DefaultText
+          style={{
+            fontSize: 22,
+            fontWeight: 900,
+            color: 'white',
+          }}
+        >
+          GIF
+        </DefaultText>
+      </View>
+    );
+  }
+
+  return (
+    <Image
+      source={image}
+      transition={150}
+      style={{
+        width: '100%',
+        aspectRatio: image.width / image.height,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        overflow: 'hidden',
+      }}
+    />
   );
 };
 
