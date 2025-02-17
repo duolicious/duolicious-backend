71859b8564b7da72c62e97933a65254def493be0
duogenesis <136373989+duogenesis@users.noreply.github.com>
2025-02-17 16:01:49 +1100

    Allow new users to send Tenor gifs (#643)

--- antiabuse/antispam/urldetector/__init__.py
+++ antiabuse/antispam/urldetector/__init__.py
@@ -2,6 +2,14 @@ from antiabuse.normalize import normalize_string
 from antiabuse.antispam.urldetector.tld import tlds
 import re
 import unicodedata
+from enum import Enum
+from typing import Tuple, List
+
+class UrlType(Enum):
+    NONE = 0
+    VERY_SAFE = 1
+    SOMEWHAT_SAFE = 2
+    UNSAFE = 3
 
 DOT_VARIATIONS_GROUP = (
     r'\s*\b(?:dot|d0t|\.|;|:|,)\b\s*|'
@@ -16,8 +24,12 @@ EXTRANEOUS_SPACES = re.compile(r'\s+')
 # A set of TLDs we consider "common" and always safe to unify
 COMMON_TLDS = {'com', 'net', 'org', 'gg', 'co', 'io'}
 
+VERY_SAFE_DOMAINS = {
+    'tenor.com',
+}
+
 # These domains are typically used to help users describe who they are
-SAFE_DOMAINS = {
+SOMEWHAT_SAFE_DOMAINS = {
     'backloggd.com',
     'facebook.com',
     'fandom.com',
@@ -32,7 +44,6 @@ SAFE_DOMAINS = {
     'stats.fm',
     'steamcommunity.com',
     'substack.com',
-    'tenor.com',
     'twitter.com',
     'vocaroo.com',
     'x.com',
@@ -56,33 +67,48 @@ UNSAFE_DOMAINS = {
 TLD_GROUP = '|'.join(map(re.escape, tlds))
 
 URL_GROUP = (
-    r"""((?:https?://|www\.)?)"""            # optional scheme or www.
+    r"""("""
+    r"""(https?://)?"""                      # optional scheme or www.
+    r"""([a-zA-Z0-9]+\.)*"""                 # optional subdomain(s)
     r"""([a-zA-Z0-9\-]+)"""                  # domain name
     rf"""({DOT_VARIATIONS_GROUP})"""         # mandatory dot
     rf"""({TLD_GROUP})(?=$|\s|[/:])"""       # known TLD
-    r"""(?:\:[0-9]+)?"""                     # optional port
-    r"""(?:/[^\s]*)?"""                      # optional path
+    r"""(\:[0-9]+)?"""                       # optional port
+    r"""(/[^\s]*)?"""                        # optional path
+    r""")"""
 )
 
 URL_PATTERN = re.compile(URL_GROUP, re.IGNORECASE)
 
-def has_url(text: str):
-    normalized_text = normalize_string(text)
+def has_url(
+    text: str,
+    include_safe: bool = False,
+    do_normalize: bool = True
+) -> List[Tuple[UrlType, str]]:
+    if do_normalize:
+        text = normalize_string(text)
 
-    matches = URL_PATTERN.findall(normalized_text)
+    matches = URL_PATTERN.findall(text)
 
-    for scheme, domain, dot, tld in matches:
-        normalized_domain = f'{domain.lower()}.{tld.lower()}'
+    def go():
+        for url, scheme, www, domain, dot, tld, port, path in matches:
+            normalized_domain = f'{domain.lower()}.{tld.lower()}'
 
-        if normalized_domain in SAFE_DOMAINS:
-            return False
-        if normalized_domain in UNSAFE_DOMAINS:
-            return True
-        if tld.lower() in COMMON_TLDS:
-            return True
-        if scheme:
-            return True
-        if not re.compile('\s+').search(dot):
-            return True
+            if normalized_domain in VERY_SAFE_DOMAINS and include_safe:
+                yield UrlType.VERY_SAFE, url
+            elif normalized_domain in VERY_SAFE_DOMAINS:
+                pass
+            elif normalized_domain in SOMEWHAT_SAFE_DOMAINS and include_safe:
+                yield UrlType.SOMEWHAT_SAFE, url
+            elif normalized_domain in SOMEWHAT_SAFE_DOMAINS:
+                pass
+            elif normalized_domain in UNSAFE_DOMAINS:
+                yield UrlType.UNSAFE, url
+            elif tld.lower() in COMMON_TLDS:
+                yield UrlType.UNSAFE, url
+            elif scheme or www:
+                yield UrlType.UNSAFE, url
+            elif not re.compile('\s+').search(dot):
+                yield UrlType.UNSAFE, url
 
-    return False
+    return list(go())
