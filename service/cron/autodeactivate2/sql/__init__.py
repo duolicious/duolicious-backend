503c6cd3407791fc03f8a6b45ee1e0e27c28180f
duogenesis <136373989+duogenesis@users.noreply.github.com>
2025-03-24 12:34:02 +1100

    autodeactivate2: 20 days

--- service/cron/autodeactivate2/sql/__init__.py
+++ service/cron/autodeactivate2/sql/__init__.py
@@ -4,9 +4,9 @@ SELECT DISTINCT
 FROM
     last
 WHERE
-    seconds < EXTRACT(EPOCH FROM NOW() - INTERVAL '30 days')
+    seconds < EXTRACT(EPOCH FROM NOW() - INTERVAL '20 days')
 AND
-    seconds > EXTRACT(EPOCH FROM NOW() - INTERVAL '50 days')
+    seconds > EXTRACT(EPOCH FROM NOW() - INTERVAL '40 days')
 """
 
 Q_DEACTIVATE = """
