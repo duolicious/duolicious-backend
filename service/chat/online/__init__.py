96e5fe4864e1bff4b86e12485c371995466e5bd6
duogenesis <136373989+duogenesis@users.noreply.github.com>
2025-05-14 04:31:41 +1000

    `within-1-day` online status (#729)

--- service/chat/online/__init__.py
+++ service/chat/online/__init__.py
@@ -5,7 +5,15 @@ from service.chat.chatutil import (
     fetch_is_skipped,
     fetch_id_from_username,
 )
+from enum import Enum
+from commonsql import Q_UPSERT_LAST
+from batcher import Batcher
+from service.chat.session import Session
+from database import api_tx
+import asyncio
+import traceback
 
+LAST_UPDATE_INTERVAL_SECONDS = 4 * 60
 
 FMT_KEY = 'online-{username}'
 
@@ -18,6 +26,12 @@ FMT_UNSUB_OK  = '<duo_unsubscribe_successful uuid="{username}" />'
 FMT_UNSUB_BAD = '<duo_unsubscribe_unsuccessful uuid="{username}" />'
 
 
+class OnlineStatus(Enum):
+    ONLINE = 'online'
+    WITHIN_ONE_DAY = 'within-1-day'
+    OFFLINE = 'offline'
+
+
 async def _redis_subscribe_online(
     redis_client: redis.Redis,
     pubsub: redis.client.PubSub,
@@ -27,7 +41,10 @@ async def _redis_subscribe_online(
     val = await redis_client.get(key)
 
     await pubsub.subscribe(key)
-    return val or FMT_ONLINE_EVENT.format(username=username, status='offline')
+    return val or FMT_ONLINE_EVENT.format(
+        username=username,
+        status=OnlineStatus.OFFLINE.value,
+    )
 
 
 async def _redis_unsubscribe_online(
@@ -42,14 +59,17 @@ async def redis_publish_online(
     username: str,
     online: bool
 ):
-    status = 'online' if online else 'offline'
+    status = (
+        OnlineStatus.ONLINE.value
+        if online
+        else OnlineStatus.WITHIN_ONE_DAY.value)
 
     key = FMT_KEY.format(username=username)
     val = FMT_ONLINE_EVENT.format(username=username, status=status)
 
     async with redis_client.pipeline(transaction=True) as pipe:
         pipe.publish(key, val)
-        pipe.set(key, val, ex=604800)  # Expires in one week
+        pipe.set(key, val, ex=86400)  # Expires in one day
         await pipe.execute()
 
 
@@ -111,3 +131,63 @@ async def maybe_redis_unsubscribe_online(
     except:
         print(traceback.format_exc())
         return [FMT_UNSUB_BAD.format(username=username)]
+
+
+
+def process_batch(usernames: list[str]):
+    params_seq = [dict(person_uuid=username) for username in usernames]
+
+    with api_tx('read committed') as tx:
+        tx.executemany(Q_UPSERT_LAST, params_seq)
+
+
+def update_last_once(session_username: str):
+    _batcher.enqueue(session_username)
+
+
+async def update_online_once(
+    redis_client: redis.Redis,
+    session: Session,
+    online: bool
+):
+    if session.username is None:
+        return
+
+    update_last_once(session_username=session.username)
+
+    await redis_publish_online(
+        redis_client=redis_client,
+        username=session.username,
+        online=online,
+    )
+
+
+async def update_online_forever(
+    redis_client: redis.Redis,
+    session: Session,
+    online: bool
+):
+    try:
+        while True:
+            await update_online_once(
+                redis_client=redis_client,
+                session=session,
+                online=online,
+            )
+            await asyncio.sleep(LAST_UPDATE_INTERVAL_SECONDS)
+    except asyncio.exceptions.CancelledError:
+        pass
+    except:
+        print(traceback.format_exc())
+        raise
+
+
+_batcher = Batcher[str](
+    process_fn=process_batch,
+    flush_interval=1.0,
+    min_batch_size=1,
+    max_batch_size=1000,
+    retry=False,
+)
+
+_batcher.start()
