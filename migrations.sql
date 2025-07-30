3707e8f71627ded4033e6bafab867a7b9d97867a
duogenesis <136373989+duogenesis@users.noreply.github.com>
2025-07-31 06:40:37 +1000

    More content types for recently online

--- migrations.sql
+++ migrations.sql
@@ -1,2 +1,4 @@
-ALTER TYPE person_event ADD VALUE IF NOT EXISTS 'was-recently-online';
+ALTER TYPE person_event ADD VALUE IF NOT EXISTS 'recently-online-with-photo';
+ALTER TYPE person_event ADD VALUE IF NOT EXISTS 'recently-online-with-voice-bio';
+ALTER TYPE person_event ADD VALUE IF NOT EXISTS 'recently-online-with-bio';
 
