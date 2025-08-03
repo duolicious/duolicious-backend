0186b4ea8eb5f245b7e11b0c6818d2c393072569
duogenesis <136373989+duogenesis@users.noreply.github.com>
2025-08-04 08:43:11 +1000

    Filter recently online events in the feed

--- migrations.sql
+++
@@ -1,4 +0,0 @@
-ALTER TYPE person_event ADD VALUE IF NOT EXISTS 'recently-online-with-photo';
-ALTER TYPE person_event ADD VALUE IF NOT EXISTS 'recently-online-with-voice-bio';
-ALTER TYPE person_event ADD VALUE IF NOT EXISTS 'recently-online-with-bio';
-
