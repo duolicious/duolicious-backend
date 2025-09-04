54b8be1cbe1cb0eeb6942e140943f794767b2d69
duogenesis <136373989+duogenesis@users.noreply.github.com>
2025-09-05 09:36:35 +1000

    Configure Revenuecat on web (#890)

--- duolicious-frontend/purchases/purchases.tsx
+++ duolicious-frontend/purchases/purchases.tsx
@@ -5,13 +5,6 @@ import Constants, { ExecutionEnvironment } from "expo-constants";
 import { notify, lastEvent } from '../events/events';
 import { getSignedInUser } from '../events/signed-in-user';
 
-// TODO: I think this should only be initialized once
-// TODO: Call these:
-//Purchases.logIn
-//Purchases.logOut
-// TODO: Configure purchases immediately before displaying the paywall, not at
-//       app start
-
 const isExpoGo =
   Constants.executionEnvironment === ExecutionEnvironment.StoreClient;
 
@@ -36,16 +29,14 @@ const ensurePurchasesConfigured = async () => {
   Purchases.setDebugLogsEnabled(isExpoGo);
 
   if (isExpoGo) {
-    await Purchases.configure({ apiKey: API_KEYS.web });
+    await Purchases.configure({ appUserID: personUuid, apiKey: API_KEYS.web });
   } else if (Platform.OS === 'android') {
-    await Purchases.configure({ apiKey: API_KEYS.google });
+    await Purchases.configure({ appUserID: personUuid, apiKey: API_KEYS.google });
   } else if (Platform.OS ==='ios')  {
-    await Purchases.configure({ apiKey: API_KEYS.apple });
-  } else if (Platform.OS === 'web' && process.env.NODE_ENV === 'development') {
-    await Purchases.configure({ apiKey: API_KEYS.web });
+    await Purchases.configure({ appUserID: personUuid, apiKey: API_KEYS.apple });
+  } else if (Platform.OS === 'web') {
+    await Purchases.configure({ appUserID: personUuid, apiKey: API_KEYS.web });
   }
-
-  await Purchases.logIn(personUuid);
 
   notify('purchases-configured', true);
 };
