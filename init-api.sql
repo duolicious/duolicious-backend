0b275f3b7f0fa4901362388b415691ffbdac7ea5
duogenesis <136373989+duogenesis@users.noreply.github.com>
2025-10-05 19:53:16 +1100

    Add `presence_histogram` table

--- init-api.sql
+++ init-api.sql
@@ -1644,6 +1644,17 @@ CREATE TABLE IF NOT EXISTS inbox (
     PRIMARY KEY(luser, remote_bare_jid)
 );
 
+-- Used to time notifications appropriately
+CREATE TABLE IF NOT EXISTS presence_histogram (
+    person_id INT REFERENCES person(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    dow SMALLINT NOT NULL, -- 0=Sun .. 6=Sat
+    hour SMALLINT NOT NULL, -- 0 .. 23 (UTC)
+    score FLOAT4 NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+
+    PRIMARY KEY (person_id, dow, hour)
+);
+
 CREATE INDEX IF NOT EXISTS
     idx__inbox__luser__timestamp
     ON inbox(luser, timestamp);
