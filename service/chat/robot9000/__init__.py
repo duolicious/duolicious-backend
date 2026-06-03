d31a4c463fd36b30d02678961df6c496253ede77
duogenesis <136373989+duogenesis@users.noreply.github.com>
2026-06-03 11:18:04 +1000

    Count non-unique intros (#958)

--- service/chat/robot9000/__init__.py
+++ service/chat/robot9000/__init__.py
@@ -1,11 +1,12 @@
 from database import api_tx
 from typing import List
 from batcher import Batcher
+from collections import Counter
 
 
 Q_SELECT_INTRO_HASH = """
 SELECT
-    1
+    used_count
 FROM
     intro_hash
 WHERE
@@ -18,20 +19,26 @@ AND
 Q_UPSERT_INTRO_HASH = """
 INSERT INTO intro_hash (
     hash,
+    used_count,
     last_used_at
 ) VALUES (
     %(hash)s,
+    %(used_count)s,
     now()
 )
 ON CONFLICT (hash) DO UPDATE SET
+    used_count = intro_hash.used_count + EXCLUDED.used_count,
     last_used_at = now()
 """
 
 
 def process_batch(batch: List[str]):
-    distinct_hashes = set(batch)
+    hash_counts = Counter(batch)
 
-    params_seq = [dict(hash=hash) for hash in distinct_hashes]
+    params_seq = [
+        dict(hash=hash, used_count=used_count)
+        for hash, used_count in hash_counts.items()
+    ]
 
     with api_tx('read committed') as tx:
         tx.executemany(Q_UPSERT_INTRO_HASH, params_seq)
