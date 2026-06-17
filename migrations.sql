8fac8ec19901e68566d44ead417f34200e239087
duogenesis <136373989+duogenesis@users.noreply.github.com>
2026-06-18 00:30:20 +1000

    Drop postgres functions (#998)

--- migrations.sql
+++
@@ -1,6 +0,0 @@
-DROP FUNCTION IF EXISTS compute_personality_vectors(INT[], INT[], INT[], INT[], INT[], INT[], SMALLINT) CASCADE;
-DROP FUNCTION IF EXISTS answer_score_vectors(INT, BOOLEAN) CASCADE;
-DROP TYPE IF EXISTS personality_vectors CASCADE;
-DROP TYPE IF EXISTS answer_score_vectors CASCADE;
-
-DROP EXTENSION IF EXISTS plpython3u;
