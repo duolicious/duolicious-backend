d65c3a830544eeeef3c1e483ee4073221daf3042
duogenesis <136373989+duogenesis@users.noreply.github.com>
2026-06-18 00:16:55 +1000

    Drop postgres functions

--- migrations.sql
+++ migrations.sql
@@ -1,9 +1,6 @@
-ALTER TABLE duo_session ADD COLUMN IF NOT EXISTS answers JSONB;
+DROP FUNCTION IF EXISTS compute_personality_vectors(INT[], INT[], INT[], INT[], INT[], INT[], SMALLINT) CASCADE;
+DROP FUNCTION IF EXISTS answer_score_vectors(INT, BOOLEAN) CASCADE;
+DROP TYPE IF EXISTS personality_vectors CASCADE;
+DROP TYPE IF EXISTS answer_score_vectors CASCADE;
 
--- TODO: Execute in a follow-up PR
--- DROP FUNCTION IF EXISTS compute_personality_vectors(INT[], INT[], INT[], INT[], INT[], INT[], SMALLINT) CASCADE;
--- DROP FUNCTION IF EXISTS answer_score_vectors(INT, BOOLEAN) CASCADE;
--- DROP TYPE IF EXISTS personality_vectors CASCADE;
--- DROP TYPE IF EXISTS answer_score_vectors CASCADE;
--- 
--- DROP EXTENSION IF EXISTS plpython3u;
+DROP EXTENSION IF EXISTS plpython3u;
